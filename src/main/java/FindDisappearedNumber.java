11f5ae071b9da9197292d122a344e6f87a806d66
shivaak <shivainfotech12@gmail.com>
2021-12-28 16:39:12 +0530

    Few more problems

---
+++ src/main/java/FindDisappearedNumber.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class FindDisappearedNumber {
+
+    public static void main(String[] args) {
+     //   System.out.println(findDisappearedNumbers(new int[]{4,3,2,7,8,2,3,1}));
+        System.out.println(findDisappearedNumbersConstantMem(new int[]{4,3,2,7,8,2,3,1}));
+    }
+
+    private static List<Integer> findDisappearedNumbersConstantMem(int[] nums) {
+        List<Integer> result = new ArrayList<Integer>();
+
+        for(int i=0;i<=nums.length-1;i++){
+            int abs = Math.abs(nums[i]);
+            if(nums[abs-1] >0) {
+                nums[abs - 1] = nums[abs - 1] * -1;
+            }
+        }
+
+        for(int i=0;i<=nums.length-1;i++){
+            if(nums[i]>0){
+                result.add(i+1);
+            }
+        }
+
+        return result;
+    }
+
+    public static List<Integer> findDisappearedNumbers(int[] nums) {
+        int[] newArr = new int[nums.length+1];
+        List<Integer> result = new ArrayList<Integer>();
+        for(int i=0;i<=nums.length-1;i++){
+            newArr[nums[i]]=1;
+        }
+        for(int i=1;i<=newArr.length-1;i++){
+            if(newArr[i]!=1){
+                result.add(i);
+            }
+        }
+        return result;
+    }
+}
--- src/main/java/MissingNumber.java
+++ src/main/java/MissingNumber.java
@@ -3,13 +3,24 @@ import java.util.Arrays;
 public class MissingNumber {
 
     public static void main(String[] args) {
-        System.out.println(missingNumber(new int[] {1,2,3}));
+        System.out.println(missingNumberBitWise(new int[] {1,3}));
     }
 
     public static int missingNumber(int[] nums) {
         int sum = Arrays.stream(nums).sum();
         int total = (nums.length * (nums.length+1))/2;
         return total-sum;
+    }
+
+    public static int missingNumberBitWise(int[] nums) {
+        /*
+         ^ -> xor
+         a^a=0
+         0^0=0
+         a^0=a
+         0^a=a
+         */
+return 0;
     }
 
 }
